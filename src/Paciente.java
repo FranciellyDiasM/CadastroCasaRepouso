02fca4d0f45dab8f6d670b63eccf5344f7af7109
trancosoWiliam <wiliam.trancoso@stone.com.br>
2023-11-26 18:33:21 -0300

    Adicionado classe Paciente

---
+++ src/Paciente.java
@@ -0,0 +1,52 @@
+public class Paciente {
+    private String nome;
+    private int anoNascimento;
+    private String genero;
+    private String alergia;
+    private String telefoneResponsavel;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAnoNascimento() {
+        return anoNascimento;
+    }
+
+    public void setAnoNascimento(int anoNascimento) {
+        this.anoNascimento = anoNascimento;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getAlergia() {
+        return alergia;
+    }
+
+    public void setAlergia(String alergia) {
+        this.alergia = alergia;
+    }
+
+    public String getTelefoneResponsavel() {
+        return telefoneResponsavel;
+    }
+
+    public void setTelefoneResponsavel(String telefoneResponsavel) {
+        this.telefoneResponsavel = telefoneResponsavel;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s\t%s\t%s\t%s\t%s\n", nome, anoNascimento, genero, alergia, telefoneResponsavel);
+    }
+}
