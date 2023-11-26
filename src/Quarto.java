e7f3c61e7b8b005f8d3b70d4a0d03787c41b31b1
trancosoWiliam <wiliam.trancoso@stone.com.br>
2023-11-26 18:29:47 -0300

    Adicionado classe Quarto

---
+++ src/Quarto.java
@@ -0,0 +1,43 @@
+public class Quarto {
+    private int numero;
+    private String tipo;
+    private String comodidades;
+    private String status;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getComodidades() {
+        return comodidades;
+    }
+
+    public void setComodidades(String comodidades) {
+        this.comodidades = comodidades;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s\t%s\t%s\t%s\n", numero, tipo, comodidades, status);
+    }
+}
